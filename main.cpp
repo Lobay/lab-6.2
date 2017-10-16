b2a269dabad7adf4e937083b57945b630b61f8d2
Lobay <32384708+Lobay@users.noreply.github.com>
2017-10-16 21:58:36 +0300

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(int argc, char* argv[]) {
+string str1 = argv[1];
+char symbol = argv [2][0];
+for(int i=0; i < str1.length(); i++)
+{
+   if (str1[i]==symbol) //str1[i]-это i-тый элемент строки
+   cout << i+1 << endl;
+}
+}
